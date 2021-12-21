27ad8a7d8838670c6cb578883b9068f3533deff0
Owen Dodd <owen.dodd@gmail.com>
2021-12-20 23:31:12 -0700

    add slideshow

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,43 +1,33 @@
 $(function () {
-    var first_slide = $(".slider>.slide:first-of-type"),
-      total_slides = $(".slider>.slide").length;
-  
-    first_slide.addClass("show");
-    $(".indicator").html("1 / " + total_slides);
-  
-    const sliders = document.querySelectorAll(".slider");
-  
-    for (let index = 0; index < sliders.length; index++) {
-      const element = sliders[index];
-      if (!element.innerHTML.trim()) {
-        element.parentElement.remove();
-      }
-    }
-  
-    $(".next").click(function () {
-      var slider = $(this).closest(".slidewrapper").children(".slider"),
-        current_slide = slider.children(".slide.show"),
+  var first_slide = $(".slider>.slide:first-of-type");
+  first_slide.addClass("show");
+
+  $(".next").click(function () {
+    var slider = $(".slider"),
+        current_slide = $(".slide.show"),
         should_loop = current_slide.next().length > 0,
         next_slide = should_loop
-          ? current_slide.next()
-          : slider.children(".slide:first");
-  
-      current_slide.removeClass("show");
-      next_slide.addClass("show");
+        ? current_slide.next()
+        : slider.children(".slide:first");
 
-    });
-  
-    $(".prev").click(function () {
-      var slider = $(this).closest(".slidewrapper").children(".slider"),
-        current_slide = slider.children(".slide.show"),
+    current_slide.removeClass("show");
+    next_slide.addClass("show");
+
+    console.log(current_slide);
+
+  });
+
+  $(".prev").click(function () {
+    var slider = $(".slider"),
+        current_slide = $(".slide.show"),
         should_loop = current_slide.prev().length > 0,
         prev_slide = should_loop
-          ? current_slide.prev()
-          : slider.children(".slide:last");
-  
-      current_slide.removeClass("show");
-      prev_slide.addClass("show");
-  
-      console.log("test");
-    });
+        ? current_slide.prev()
+        : slider.children(".slide:last");
+
+    current_slide.removeClass("show");
+    prev_slide.addClass("show");
+
+    console.log("forward");
   });
+});
