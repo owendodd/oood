aed0f05c199d3bca66e95c6d5097d827bcee488a
Owen Dodd <owen.dodd@gmail.com>
2021-12-21 09:55:50 -0700

    push new nav

--- static/js/nav.js
+++ static/js/nav.js
@@ -3,7 +3,7 @@ $(function () {
     $(".nav").hover(function () {
         $(".menugroup").toggleClass("show");
         $(".menuicon .bar:nth-of-type(2)").toggleClass("twothird");
-        $(".menuicon .bar:nth-of-type(3)").toggleClass("onethird");
+        // $(".menuicon .bar:nth-of-type(3)").toggleclass("onethird");
     });
   
   });
